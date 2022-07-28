57ce09d1af0fe8b0bc15687afe9d4b137ae65e08
Joons <elddy0948@gmail.com>
2022-07-29 01:39:33 +0900

    Implementation ProductResponse DTO

---
+++ Befam/Befam/Data/Network/DataMapping/ProductResponseDTO.swift
@@ -0,0 +1,57 @@
+import Foundation
+
+struct ProductResponseDTO: Decodable {
+  let resultCount: Int
+  let results: [ProductDTO]
+}
+
+extension ProductResponseDTO {
+  struct ProductDTO: Decodable {
+    let trackName: String
+    let sellerName: String
+    
+    let userRatingCount: Int
+    let averageUserRating: Double
+    
+    let trackContentRating: String
+    let genres: [String]
+    
+    let artworkUrl60: String
+    let artworkUrl512: String
+    let artworkUrl100: String
+    
+    let screenshotUrls: [String]
+    
+    let currentVersionReleaseDate: String
+    let releaseNotes: String
+    
+    let fileSizeBytes: String
+    let version: String
+    
+    let description: String
+    let languageCodesISO2A: [String]
+  }
+}
+
+extension ProductResponseDTO.ProductDTO {
+  func toDomain() -> Product {
+    return Product(
+      trackName: trackName,
+      sellerName: sellerName,
+      userRatingCount: userRatingCount,
+      averageUserRating: averageUserRating,
+      trackContentRating: trackContentRating,
+      genres: genres,
+      artworkUrl60: artworkUrl60,
+      artworkUrl512: artworkUrl512,
+      artworkUrl100: artworkUrl100,
+      screenshotUrls: screenshotUrls,
+      currentVersionReleaseDate: currentVersionReleaseDate,
+      releaseNotes: releaseNotes,
+      fileSizeBytes: fileSizeBytes,
+      version: version,
+      description: description,
+      languageCodesISO2A: languageCodesISO2A
+    )
+  }
+}
