74653a4e846669fb19df782091391d22ba283c98
Joons <elddy0948@gmail.com>
2022-07-29 14:59:38 +0900

    Implementation CoreDataStack

---
+++ Befam/Befam/Data/PersistentStorage/CoreDataStorage/CoreDataStack.swift
@@ -0,0 +1,30 @@
+import CoreData
+
+final class CoreDataStack {
+  static let shared = CoreDataStack()
+  
+  lazy var managedContext: NSManagedObjectContext = {
+    return self.persistentContainer.viewContext
+  }()
+  
+  private lazy var persistentContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: "ProductModel")
+    container.loadPersistentStores(completionHandler: { _, error in
+      if let error = error {
+        fatalError(error.localizedDescription)
+      }
+    })
+    return container
+  }()
+  
+  private init() {}
+  
+  func saveContext() {
+    guard managedContext.hasChanges else { return }
+    do {
+      try managedContext.save()
+    } catch {
+      print("Failed to save in CoreData : \(error.localizedDescription)")
+    }
+  }
+}
